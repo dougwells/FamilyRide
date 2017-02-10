491efc92564cf1c7c9e727a9a34be55a9daa712b
dougwells <dougwells@gmail.com>
2017-02-10 15:40:40 -0700

    uberClone.  Add mapview. Currently static location.  Next - show current location

--- uberClone/riderMapViewController.swift
+++ uberClone/riderMapViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import MapKit
 import Parse
 
-class riderMapViewController: UIViewController {
+class riderMapViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
 
     @IBAction func logoutButton(_ sender: UIBarButtonItem) {
         
@@ -19,6 +22,26 @@ class riderMapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Set vars to make code cleaner.  Note: CL stands for "Call Location"
+        let latitude: CLLocationDegrees = 40.6461
+        let longitude: CLLocationDegrees = -111.4980
+        let latDelta: CLLocationDegrees = 0.03	//amnt of lat/lon in set amount of space
+        let lonDelta: CLLocationDegrees = 0.03	//lower = less distance so more “zoom”
+        
+        //Sets "zoom" level
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        
+        //Sets location
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        //Sets initial "region" (location and zoom level with vars "location" & "span")
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        //Finally, time to tell iOS where in map to set initial location and zoom level
+        //Technically, could simply use this line but code would not be clear ...
+        map.setRegion(region, animated: true)
+
 
         // Do any additional setup after loading the view.
     }
