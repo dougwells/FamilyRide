1fbd223c69395da8084bc7308873893478de582b
dougwells <dougwells@gmail.com>
2017-02-12 15:55:21 -0700

    uberClone. Add ability to manually update uber pickup location.

--- uberClone/riderMapViewController.swift
+++ uberClone/riderMapViewController.swift
@@ -17,74 +17,54 @@ class riderMapViewController: UIViewController, MKMapViewDelegate, CLLocationMan
     var existingUberRequest = false
     var latitude: CLLocationDegrees = 0.0
     var longitude: CLLocationDegrees = 0.0
+    let annotation = MKPointAnnotation()
     @IBOutlet weak var map: MKMapView!
     @IBOutlet weak var callAnUberButton: UIButton!
-    
-    func createAlert(title: String, message: String ) {
-        //creat alert
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
-        
-        //add button to alert
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        
-        //present alert
-        self.present(alert, animated: true, completion: nil)
-    }  //End createAlert
     
     
     @IBAction func callAnUber(_ sender: UIButton) {
         
-        if !existingUberRequest {
-            existingUberRequest = true
-            sender.setTitle("Cancel Uber", for: [])
-            
-            PFGeoPoint.geoPointForCurrentLocation { (geopoint, error) in
-                self.createAlert(title: "Uber", message: "Your pickup request has been made.")
-                print("Uber requested. PFGeopoint =", geopoint)
-                if let geopoint = geopoint {
-                    
-                    let riderRequest = PFObject(className: "RiderRequest")
-                    
-                    riderRequest["location"] = geopoint as? PFGeoPoint
-                    riderRequest["username"] = PFUser.current()?.username
-                    riderRequest["riderId"] = PFUser.current()?.objectId
-                    riderRequest.saveInBackground()
-                }
-            }
-        } else {
-            existingUberRequest = false
-            sender.setTitle("Call an Uber", for: [])
-            self.createAlert(title: "Uber", message: "Your Uber request has been cancelled.")
-            let query = PFQuery(className: "RiderRequest")
-            query.whereKey("riderId", equalTo: PFUser.current()?.objectId)
-            query.findObjectsInBackground(block: { (objects, error) in
-                if let requests = objects {
-                    for object in requests {
-                        if let request = object as? PFObject {
-                            request.deleteInBackground()
-                        }
-                    }
-                }
-            })
+        if latitude == 0.0 && longitude == 0.0 {
+            self.createAlert(title: "Uber", message: "Unable to access your location. Please allow app to access location.")
         }
         
-        
-        
+        if !existingUberRequest {
+            
+            self.makeNewUberRequest()
+            
+        } else {
+            
+            self.cancelExistingUberRequest()
+            
+        }
     }
     
 
     @IBAction func logoutButton(_ sender: UIBarButtonItem) {
+        
         print("Logging out \(PFUser.current()?.username).")
+        
         self.locationManager.stopUpdatingLocation()
-        self.performSegue(withIdentifier: "riderMapToLogin", sender: self)
+        
+        self.cancelExistingUberRequest()
+        
         PFUser.logOutInBackground(block: { (error) in
+            
             if error != nil {
+                
                 print("error logging out user \(PFUser.current()?.username)")
+                
             } else {
+                
                 print("logged out user \(PFUser.current()?.username)")
+                
+                self.performSegue(withIdentifier: "riderMapToLogin", sender: self)
+                
             }
         })
     } //end logoutButton
+    
+    
     
     
     override func viewDidLoad() {
@@ -112,8 +92,8 @@ class riderMapViewController: UIViewController, MKMapViewDelegate, CLLocationMan
             print("user location =", latitude, longitude)
         }
 
-        let latDelta: CLLocationDegrees = 0.02
-        let lonDelta: CLLocationDegrees = 0.02
+        let latDelta: CLLocationDegrees = 0.01
+        let lonDelta: CLLocationDegrees = 0.01
         
         //Sets "zoom" level
         let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
@@ -126,34 +106,108 @@ class riderMapViewController: UIViewController, MKMapViewDelegate, CLLocationMan
         
         //Finally, time to tell iOS where in map to set initial location and zoom level
         self.map.setRegion(region, animated: true)
-        //saveCurrUserLocation()
         
-        //set annotation (delete prev annot & create new one)
+        
+        //update pickup location
+            //self.updateUberPickupLocation()
+    }
+    
+    func createAlert(title: String, message: String ) {
+        //creat alert
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        //add button to alert
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        //present alert
+        self.present(alert, animated: true, completion: nil)
+    }  //End createAlert
+    
+    func showAnnotation() {
         if map.annotations.count != 0 {
             map.removeAnnotations(map.annotations)
         }
-        let annotation = MKPointAnnotation()
         annotation.coordinate.latitude = latitude
         annotation.coordinate.longitude = longitude
-        annotation.title = "Rider location"
-        self.map.addAnnotation(annotation)
+        annotation.title = "Pickup location"
+        map.addAnnotation(annotation)
     }
     
+    func makeNewUberRequest() {
+        existingUberRequest = true
+        callAnUberButton.setTitle("Cancel Uber", for: [])
     
-    
-    func saveCurrUserLocation() {
-        //find user location (need to add "Privacy - Location when in use in plist for PFGeopoint to work
-        
         PFGeoPoint.geoPointForCurrentLocation { (geopoint, error) in
-            print("saveCurrUserLocation returned. PFGeopoint =", geopoint)
+            self.createAlert(title: "Uber", message: "Your pickup request has been made.")
+            print("Uber requested. PFGeopoint =", geopoint)
             if let geopoint = geopoint {
                 
+                self.showAnnotation()
                 let riderRequest = PFObject(className: "RiderRequest")
-                
                 riderRequest["location"] = geopoint as? PFGeoPoint
                 riderRequest["username"] = PFUser.current()?.username
                 riderRequest["riderId"] = PFUser.current()?.objectId
                 riderRequest.saveInBackground()
+            }
+        }
+    }
+    @IBAction func updatePickupLocation(_ sender: UIBarButtonItem) {
+        
+        self.updateUberPickupLocation()
+    }
+    
+    func cancelExistingUberRequest() {
+        existingUberRequest = false
+        
+        if map.annotations.count != 0 {
+            map.removeAnnotations(map.annotations)
+        }
+        
+        callAnUberButton.setTitle("Call an Uber", for: [])
+        self.createAlert(title: "Uber", message: "Your Uber request has been cancelled.")
+        
+        let query = PFQuery(className: "RiderRequest")
+        query.whereKey("riderId", equalTo: PFUser.current()?.objectId)
+        query.findObjectsInBackground(block: { (objects, error) in
+            if let requests = objects {
+                for object in requests {
+                    if let request = object as? PFObject {
+                        request.deleteInBackground()
+                    }
+                }
+            }
+        })
+    }
+    
+    
+    
+    func updateUberPickupLocation() {
+        
+        PFGeoPoint.geoPointForCurrentLocation { (geopoint, error) in
+            
+            if let geopoint = geopoint {
+                
+                let query = PFQuery(className: "RiderRequest")
+                query.whereKey("riderId", equalTo: PFUser.current()?.objectId)
+                
+                query.findObjectsInBackground(block: { (objects, error) in
+                    if let requests = objects {
+                        if  requests.count == 0 {
+                            self.createAlert(title: "Unable to update pickup location.", message: "Please cancel existing Uber & make new request.")
+                            print("Update Pickup error:", error)
+                        } else {
+                            for object in requests {
+                                if let request = object as? PFObject {
+                                    request["location"] = geopoint as? PFGeoPoint
+                                    self.showAnnotation()
+                                    print("Pickup location updated")
+                                    request.saveInBackground()
+                                }
+                            }
+                        }
+
+                    }
+                })
             }
         }
     }
