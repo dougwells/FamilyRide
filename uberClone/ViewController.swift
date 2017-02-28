4a980facd95eccd572829c87f89672bde119ef75
dougwells <dougwells@gmail.com>
2017-02-27 20:17:38 -0700

    familyRide. Minor comment changes

--- uberClone/ViewController.swift
+++ uberClone/ViewController.swift
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) 2015-present, Parse, LLC.
+* Copyright (c) 2017-present, Doug Wells
 * All rights reserved.
 *
 * This source code is licensed under the BSD-style license found in the
